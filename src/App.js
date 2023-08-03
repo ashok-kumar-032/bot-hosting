1a603cacc5c2411aad5da1676bf21c37d056d307
ashok-kumar-032 <av6017819@gmail.com>
2023-08-03 12:33:51 +0530

    folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Helper from "./components/common/Helper";
 import Mynav from "./components/Mynav";
 
 function App() {
   return (
     <div className="App">
+      <Home />
+      <Product />
+      <Helper />
       <Mynav />
     </div>
   );
