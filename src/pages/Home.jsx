30a5a5c53641e28a25c64d1f807e9f4c90ae7ee7
ashok-kumar-032 <av6017819@gmail.com>
2023-08-03 12:24:58 +0530

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,8 @@
-import React from "react";
-
-const Home = () => {
-  return <div>Home</div>;
-};
-
-export default Home;
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home
-      lsdkhsbjfjsdgf
-      sd;lfkhidsbfkjs
-      slkfhiuskjfd
-      kjbuds
-      
-    </div>
+    <div>Home</div>
   )
 }
 
