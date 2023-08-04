8a7c547ffb5c0dc603a5b79c88e398492fee61bf
Your Name <sumitkhan.dhana@gmail.com>
2023-08-04 09:42:57 +0530

    choose add push

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Feature from "./components/product/Feature";
 import {feature} from "./components/common/Helper"
 import {modpack} from "./components/common/Helper"
 import ModPAck from "./components/product/ModPAck";
+import ChooseUs from "./components/product/ChooseUs";
+import {Choose} from "./components/common/Helper";
 function App() {
   return (
     <>
     <Feature feature={feature}/>
     <ModPAck modpack={modpack}/>
+    <ChooseUs ChooseUs={Choose}/>
     </>
   );
 }
--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,3 +1,4 @@
+// features section images
 import f_img1 from "../../assets/img/png/f_1.png";
 import f_img2 from "../../assets/img/png/f_2.png";
 import f_img3 from "../../assets/img/png/f_3.png";
@@ -11,9 +12,17 @@ import f_img10 from "../../assets/img/png/f_10.png";
 import f_img11 from "../../assets/img/png/f_11.png";
 import f_img12 from "../../assets/img/png/f_12.png";
 import r_arrow from "../../assets/img/png/right_arrow.png";
-import mod_1 from "../../assets/img/png/modpack1.png";
 import btn_arrow from "../../assets/img/png/started_r_arrow.png";
-// modpack images
+// modpack section images
+import mod_1 from "../../assets/img/png/modpack1.png";
+import mod_2 from "../../assets/img/png/modpack2.png";
+import mod_3 from "../../assets/img/png/modpack3.png";
+import mod_4 from "../../assets/img/png/modpack4.png";
+import mod_5 from "../../assets/img/png/modpack5.png";
+import mod_6 from "../../assets/img/png/modpack6.png";
+// chooseus images
+import massage_icon from "../../assets/img/png/message_icon.png";
+// features map data start
 export const feature = [
   {
     f_img: f_img1,
@@ -76,16 +85,149 @@ export const feature = [
     arrow: r_arrow,
   },
 ];
+// features map data end
 
-export const modpack = [{
-    modimg:mod_1,
-    bigheading:"Minecraft Vanilla",
-    bluepara:"2 GB recommended",
-    whitepara:"Minecraft Vanilla is the official Minecraft version from Mojang.",
-    smallheading:"$49.99",
-    popularbtn:"Popular",
-    officalbtn:"Official",
-    startup:"Get Started",
-    rightarrow:btn_arrow,
-
-}];
+// modpack map data start
+export const modpack = [
+  {
+    modimg: mod_1,
+    bigheading: "Minecraft Vanilla",
+    bluepara: "2 GB recommended",
+    whitepara:
+      "Minecraft Vanilla is the official Minecraft version from Mojang.",
+    smallheading: "$49.99",
+    popularbtn: "Popular",
+    officalbtn: "Official",
+    startup: "Get Started",
+    rightarrow: btn_arrow,
+  },
+  {
+    modimg: mod_2,
+    bigheading: "Minecraft Vanilla",
+    bluepara: "2 GB recommended",
+    whitepara:
+      "Minecraft Vanilla is the official Minecraft version from Mojang.",
+    smallheading: "$49.99",
+    popularbtn: "Popular",
+    officalbtn: "Official",
+    startup: "Get Started",
+    rightarrow: btn_arrow,
+  },
+  {
+    modimg: mod_3,
+    bigheading: "Minecraft Vanilla",
+    bluepara: "2 GB recommended",
+    whitepara:
+      "Minecraft Vanilla is the official Minecraft version from Mojang.",
+    smallheading: "$49.99",
+    popularbtn: "Popular",
+    officalbtn: "Official",
+    startup: "Get Started",
+    rightarrow: btn_arrow,
+  },
+  {
+    modimg: mod_4,
+    bigheading: "Minecraft Vanilla",
+    bluepara: "2 GB recommended",
+    whitepara:
+      "Minecraft Vanilla is the official Minecraft version from Mojang.",
+    smallheading: "$49.99",
+    popularbtn: "Popular",
+    officalbtn: "Official",
+    startup: "Get Started",
+    rightarrow: btn_arrow,
+  },
+  {
+    modimg: mod_5,
+    bigheading: "Minecraft Vanilla",
+    bluepara: "2 GB recommended",
+    whitepara:
+      "Minecraft Vanilla is the official Minecraft version from Mojang.",
+    smallheading: "$49.99",
+    popularbtn: "Popular",
+    officalbtn: "Official",
+    startup: "Get Started",
+    rightarrow: btn_arrow,
+  },
+  {
+    modimg: mod_6,
+    bigheading: "Minecraft Vanilla",
+    bluepara: "2 GB recommended",
+    whitepara:
+      "Minecraft Vanilla is the official Minecraft version from Mojang.",
+    smallheading: "$49.99",
+    popularbtn: "Popular",
+    officalbtn: "Official",
+    startup: "Get Started",
+    rightarrow: btn_arrow,
+  },
+];
+// modpack map data end
+export const Choose = [
+  {
+    choose_card: "choose_darkbg",
+    bigheading: "Enterprise Hardware",
+    para1:
+      "Sales Cloud wurde mit der Mission geboren, Gründern von B2B-Start-ups dabei zu helfen, ihre",
+    para2:
+      "Verkaufsorganisationen auf eine kapitaleffiziente Weise aufzubauen und zu skalieren",
+    massage: massage_icon,
+    smallheading: "explore more",
+    arrow: r_arrow,
+  },
+  {
+    choose_card: "choose_lightbg",
+    bigheading: "Budget Friendly",
+    para1:
+      "Sales Cloud wurde mit der Mission geboren, Gründern von B2B-Start-ups dabei zu helfen, ihre",
+    para2:
+      "Verkaufsorganisationen auf eine kapitaleffiziente Weise aufzubauen und zu skalieren",
+    massage: massage_icon,
+    smallheading: "explore more",
+    arrow: r_arrow,
+  },
+  {
+    choose_card: "choose_darkbg ",
+    bigheading: "Amazing Support",
+    para1:
+      "Sales Cloud wurde mit der Mission geboren, Gründern von B2B-Start-ups dabei zu helfen, ihre",
+    para2:
+      "Verkaufsorganisationen auf eine kapitaleffiziente Weise aufzubauen und zu skalieren",
+    massage: massage_icon,
+    smallheading: "explore more",
+    arrow: r_arrow,
+  },
+  {
+    choose_card: "choose_lightbg ",
+    bigheading: "Premium Locations",
+    para1:
+      "Sales Cloud wurde mit der Mission geboren, Gründern von B2B-Start-ups dabei zu helfen, ihre",
+    para2:
+      "Verkaufsorganisationen auf eine kapitaleffiziente Weise aufzubauen und zu skalieren",
+    massage: massage_icon,
+    smallheading: "explore more",
+    arrow: r_arrow,
+  },
+  {
+    choose_card: "choose_darkbg ",
+    bigheading: "EInstant Setup",
+    para1:
+      "Sales Cloud wurde mit der Mission geboren, Gründern von B2B-Start-ups dabei zu helfen, ihre",
+    para2:
+      "Verkaufsorganisationen auf eine kapitaleffiziente Weise aufzubauen und zu skalieren",
+    massage: massage_icon,
+    smallheading: "explore more",
+    arrow: r_arrow,
+  },
+  {
+    choose_card: "choose_lightbg ",
+    bigheading: "DDoS Protection",
+    para1:
+      "Sales Cloud wurde mit der Mission geboren, Gründern von B2B-Start-ups dabei zu helfen, ihre",
+    para2:
+      "Verkaufsorganisationen auf eine kapitaleffiziente Weise aufzubauen und zu skalieren",
+    massage: massage_icon,
+    smallheading: "explore more",
+    arrow: r_arrow,
+  },
+];
