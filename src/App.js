ae6095c774931e1c6ca5a8c315ecc1c9781a6079
ashok-kumar-032 <av6017819@gmail.com>
2023-08-04 09:21:13 +0530

    pull

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import Feature from "./components/product/Feature";
 import { feature } from "./components/common/Helper";
 import ModPAck from "./components/product/ModPAck";
-import Game from "./components/common/Game";
+import Hosting from "./components/common/Hosting";
 function App() {
   return (
     <>
       <Feature feature={feature} />
       <ModPAck />
-      <Game />
+      <Hosting />
     </>
   );
 }
